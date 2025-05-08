512590834d60e681094a1aa88e4e95f70973451f
fezin <2411550401@fema.edu.br>
2025-05-08 18:18:16 -0300

    feat: implement stored value

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -2,12 +2,16 @@ import type { UserInterface } from "@/types/UserInterface"
 import { createContext, useEffect, useState, type Dispatch, type ReactNode, type SetStateAction } from "react"
 
 interface UserContextItens {
+    theme: string | "light" | "dark";
+    toggleTheme: () => void
     user: UserInterface
     setUser: Dispatch<SetStateAction<UserInterface>>
 }
 
 const DEFAULT_VALUES: UserContextItens = {
     setUser: () => {},
+    theme: "light",
+    toggleTheme: () => {},
     user: {
         email: "",
         id: 0,
@@ -19,6 +23,8 @@ const UserContext = createContext<UserContextItens>(DEFAULT_VALUES)
 
 const UserContextProvider = ({ children }: {children: ReactNode}) => {
     const STORED_USER = localStorage.getItem("user")
+    const STORED_THEME = localStorage.getItem("theme")
+    const [theme, setTheme] = useState<string>(STORED_THEME ? STORED_THEME : "light")
     const [user, setUser] = useState<UserInterface>(STORED_USER ? JSON.parse(STORED_USER) : {
         email: "",
         id: 0,
@@ -29,8 +35,20 @@ const UserContextProvider = ({ children }: {children: ReactNode}) => {
         user && user.id && localStorage.setItem("user", JSON.stringify(user))
     }, [user])
 
+    useEffect(() => {
+        localStorage.setItem("theme", theme)
+    }, [theme])
+
+    function toggleTheme(){
+        if(theme === "light"){
+            setTheme("dark")
+        } else {
+            setTheme("light")
+        }
+    }
+
     return (
-        <UserContext.Provider value={{ user, setUser }}>
+        <UserContext.Provider value={{ user, setUser, theme, toggleTheme }}>
             {children}
         </UserContext.Provider>
     )
