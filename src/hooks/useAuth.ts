f8bd0e1533634285360b1506d51f29946ac4af3b
fezin <2411550401@fema.edu.br>
2025-05-09 01:39:18 -0300

    feat: add function to handle with register logic

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -8,6 +8,7 @@ import { AuthContext } from "@/context/AuthContext"
 import type { loginFormType } from "@/pages/login-page/LoginPage"
 
 import { toast } from "sonner"
+import type { registerFormType } from "@/pages/register-page/RegisterPage"
 
 const authService = new AuthService()
 
@@ -15,24 +16,37 @@ const useAuth = () => {
     const navigate = useNavigate()
     const { login: contextLoginMethod, logout: contextLogoutMethod } = useContext(AuthContext)
 
-    async function login({ email, password }: loginFormType) {
+    async function register(newUser: registerFormType) {
         try {
-            const { loggedUser, message, token } = await authService.login({ email, password })
+            const { message, status } = await authService.register(newUser)
+
+            if (status === 201) {
+                login({ email: newUser.email, password: newUser.password }, message)
+            }
+
+        } catch (e: any) {
+            toast.error(e.message)
+        }
+    }
+
+    async function login({ email, password }: loginFormType, registerMessage?: string) {
+        try {
+            const { loggedUser, message: loginMessage, token } = await authService.login({ email, password })
             contextLoginMethod(token, loggedUser)
-            toast.success(message)
+            toast.success(registerMessage ? registerMessage : loginMessage)
             navigate("/")
         } catch (e: any) {
             toast.error(e.message)
         }
     }
 
-    async function logout(){
+    async function logout() {
         contextLogoutMethod()
         toast.success("Saindo...")
         navigate("/login")
     }
 
-    return { login, logout }
+    return { login, logout, register }
 }
 
 export default useAuth
