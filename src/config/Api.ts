6d20b36707de04f5db0c7b9d10e68e0cda6cbac7
fezin <2411550401@fema.edu.br>
2025-05-09 18:36:39 -0300

    hotfix: ultimo hotfix prometo

--- src/config/Api.ts
+++ src/config/Api.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 const STORED_TOKEN = localStorage.getItem("token")
 
 const Api = axios.create({
-    baseURL: "https://bb34-2804-7f0-451-a73b-6f3f-9235-d994-6150.ngrok-free.app",
+    baseURL: "https://bb34-2804-7f0-451-a73b-6f3f-9235-d994-6150.ngrok-free.app:8080",
     headers: {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${STORED_TOKEN}`
