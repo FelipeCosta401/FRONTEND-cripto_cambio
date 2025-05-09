e8421092474770a32d02d3e1ede26c04191f1e80
fezin <2411550401@fema.edu.br>
2025-05-08 23:23:50 -0300

    feat: add method to handle with favorite coin at coinService

--- src/services/CoinService.ts
+++ src/services/CoinService.ts
@@ -1,7 +1,7 @@
 import CoinGekkoApi from "@/config/CoinGekkoApi"
+import Api from "@/config/Api"
+
 import type { CoinGekkoInterface } from "@/types/CoinInterface"
-
-
 
 export default class CoinService {
 
@@ -14,6 +14,26 @@ export default class CoinService {
             }).catch((error) => {
                 console.log(error)
                 throw new Error("Erro ao buscar moedas!")
+            })
+    }
+
+    async handleFavorite(coinSymbol: string) {
+        return await Api.post("/favorites", {
+            coinSymbol
+        })
+            .then((res: { data: { updatedFavoritesCoinsList: { coinSymbol: string }[] } }) => {
+                const { updatedFavoritesCoinsList: favoriteList } = res.data
+
+                const updatedFavoritesCoinsList: string[] = []
+
+                favoriteList.forEach((coin) => {
+                    updatedFavoritesCoinsList.push(coin.coinSymbol)
+                })
+
+                return updatedFavoritesCoinsList
+            }).catch((error) => {
+                console.log(error)
+                throw new Error(error.response.data.error)
             })
     }
 
